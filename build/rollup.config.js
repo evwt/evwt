2397eb351dc191d55290f51ede8cdbdb13606421
John Susek <john@johnsolo.net>
2020-08-07 08:52:50 -0500

    Add some rollup config to work with electron-store.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,10 +1,13 @@
 import path from 'path';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import vue from 'rollup-plugin-vue';
 import css from 'rollup-plugin-css-only';
 import scss from 'rollup-plugin-scss';
 import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
+import builtins from 'builtin-modules';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 let customResolver = resolve({
   extensions: ['.js', '.scss', '.vue']
@@ -18,11 +21,14 @@ export default {
     name: 'evwt',
     exports: 'named',
     globals: {
-      'electron': 'electron'
+      electron: 'electron',
+      path: 'path',
+      fs: 'fs'
     }
   },
-  external: ['electron'],
+  external: ['electron', ...builtins],
   plugins: [
+    nodePolyfills(),
     alias({
       entries: [
         {
@@ -32,8 +38,11 @@ export default {
       ],
       customResolver
     }),
-    resolve(),
+    resolve({
+      preferBuiltins: true
+    }),
     commonjs(),
+    json(),
     css(),
     scss(),
     vue()
